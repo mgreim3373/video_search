fd94d776f701d2972512ea4cbe4ed076d63865ec
mgreim3373 <mgreim3373@gmail.com>
2019-03-31 23:25:29 -0400

    Add Video List, Video Items and Video Detail -create video player with iFrame in VideoDetail component -create list of videoItems in VideoList via map -create video elements in videoItem component

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,11 @@ import React from 'react'
 import SearchBar from './SearchBar'
 import youtube from '../apis/youtube'
 import './SearchBar.css'
+import VideoList from './VideoList'
+import VideoDetail from './VideoDetail'
 
 class App extends React.Component {
-  state = { videos: [] }
+  state = { videos: [], selectedVideo: null }
   onTermSubmit = async (term) => {
     const response = await youtube.get('/search', {
       params: {
@@ -12,12 +14,30 @@ class App extends React.Component {
       }
     })
 
-    this.setState({ videos: response.data.items})
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0]
+    })
   }
+
+  onVideoSelect = (video) => {
+    this.setState({ selectedVideo: video })
+  }
+
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit}/>
+        <div className='ui grid'>
+          <div className='ui row'>
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className='five wide column'>'
+              <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos}/>
+            </div>
+          </div>
+        </div>
       </div>
         // <div className="image-container">
         // <div className="colorPlane"></div>
